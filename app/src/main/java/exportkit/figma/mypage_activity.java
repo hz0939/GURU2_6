c86de9b44d44fc00777b991d7eaa4c8671e01af7
Seolhyekyeong <totomn@naver.com>
2023-07-25 12:18:08 +0900

    디자인 적용

---
+++ app/src/main/java/exportkit/figma/mypage_activity.java
@@ -0,0 +1,80 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		mypage
+	 *	@date 		Tuesday 25th of July 2023 02:02:37 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class mypage_activity extends Activity {
+
+	
+	private View _bg__mypage;
+	private View ellipse_3;
+	private View rectangle_36;
+	private TextView t52ttt;
+	private TextView t51ttt;
+	private TextView t50ttt;
+	private TextView t49ttt;
+	private TextView t48ttt;
+	private ImageView backicon8;
+	private TextView t47ttt;
+	private View rectangle_34;
+	private TextView t46ttt;
+	private TextView t45ttt;
+	private TextView t44ttt;
+	private View rectangle_35;
+	private View rectangle_342;
+	private ImageView myicon;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mypage);
+
+		
+		_bg__mypage = (View) findViewById(R.id._bg__mypage);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		rectangle_36 = (View) findViewById(R.id.rectangle_36);
+		t52ttt = (TextView) findViewById(R.id.t52ttt);
+		t51ttt = (TextView) findViewById(R.id.t51ttt);
+		t50ttt = (TextView) findViewById(R.id.t50ttt);
+		t49ttt = (TextView) findViewById(R.id.t49ttt);
+		t48ttt = (TextView) findViewById(R.id.t48ttt);
+		backicon8 = (ImageView) findViewById(R.id.backicon8);
+		t47ttt = (TextView) findViewById(R.id.t47ttt);
+		rectangle_34 = (View) findViewById(R.id.rectangle_34);
+		t46ttt = (TextView) findViewById(R.id.t46ttt);
+		t45ttt = (TextView) findViewById(R.id.t45ttt);
+		t44ttt = (TextView) findViewById(R.id.t44ttt);
+		rectangle_35 = (View) findViewById(R.id.rectangle_35);
+		rectangle_342 = (View) findViewById(R.id.rectangle_342);
+		myicon = (ImageView) findViewById(R.id.myicon);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
